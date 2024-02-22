3df1520316df4462284358166368db670c4d4e14
EMBEDDED <EMBEDDED@EMBEDDED-179>
2024-02-22 15:58:56 +0900

    얼굴에서 눈 인식, 얼굴 합성 추가

--- MFCApplication1/resource.h
+++ MFCApplication1/resource.h
@@ -60,6 +60,7 @@
 #define IDC_CANVAS                      1029
 #define IDC_STATIC7                     1030
 #define IDC_SLIDER2                     1031
+#define IDC_MERGE                       1032
 
 // Next default values for new objects
 // 
@@ -67,7 +68,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        137
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1032
+#define _APS_NEXT_CONTROL_VALUE         1033
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
